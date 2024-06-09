36782dfc495712f9f40bc0a81f9184bcad669149
erick-sudo <erickochieng766@gmail.com>
2024-06-09 20:27:13 +0300

    Allowed Hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "render-t-5tm8.onrender.com"
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
