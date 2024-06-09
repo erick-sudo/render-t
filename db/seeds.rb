a6cb4c02de9079d98311a7e351aad242fdb616dd
erick-sudo <erickochieng766@gmail.com>
2024-06-09 19:56:18 +0300

    Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Seeding conversations"
+(1..5).each do
+  Conversation.create! title: Faker::Lorem.sentence
+
+end
+puts "Done seeding conversations"
